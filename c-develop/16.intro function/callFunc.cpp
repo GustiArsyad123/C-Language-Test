0e27e1d276ebbd6ba4531141eb528dcf22ae769d
@suriv27121994 <suriv2712194@gmail.com>
2021-12-28 07:55:04 +0800

    add call function

---
+++ c-develop/16.intro function/callFunc.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void thisFungsi()
+{
+    cout << "This is function!\n";
+}
+
+int main()
+{
+    thisFungsi();
+    thisFungsi();
+    thisFungsi();
+    return 0;
+}
+
+// In below is output from iniFungsi 
+// This is function!
+// This is function!
+// This is function!
