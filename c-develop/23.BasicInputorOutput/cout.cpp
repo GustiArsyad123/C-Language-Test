7b7d8403d9ee109f7c28332e6ead896713ac0719
@suriv27121994 <suriv2712194@gmail.com>
2021-12-27 20:59:24 +0800

    The Standard Output Stream(cout)

---
+++ c-develop/23.BasicInputorOutput/cout.cpp
@@ -0,0 +1,9 @@
+// The Standard Output Stream(cout)
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char str[] = "Hello World, and this C++"; // str is string
+    cout << "Value of str is : " << str << endl;
+}
