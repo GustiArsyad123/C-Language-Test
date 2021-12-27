eec7dddd950c669b3c8e2a23a23b066a2016db8a
@suriv27121994 <suriv2712194@gmail.com>
2021-12-27 20:56:44 +0800

    The Standard Input Stream(cin)

---
+++ c-develop/23.BasicInputorOutput/basicInputOrOut.cpp
@@ -0,0 +1,12 @@
+// The Standard Input Stream(cin)
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char nm[30]; // nm is name
+
+    cout << "Please enter your name: ";
+    cin >> nm;
+    cout << "Your name is: " << nm << endl;
+}
