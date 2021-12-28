59284d60b6b64c24d3411696d95d461c8be54f46
@suriv27121994 <suriv2712194@gmail.com>
2021-12-28 08:20:50 +0800

    add do while loop

---
+++ c-develop/24. While Loop/doWhile.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 0;
+    do
+    {
+        cout << i << "\n";
+        i++;
+    } while (i < 5); // condition
+}
+
+/**
+ *
+ do {
+  // code block to be executed
+}
+while (condition);
+*
+*/
