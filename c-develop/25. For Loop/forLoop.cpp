a947b7bc5c02feb9a214f063eb937ca0ef5027a2
@suriv27121994 <suriv2712194@gmail.com>
2021-12-28 08:29:19 +0800

    add for loop

---
+++ c-develop/25. For Loop/forLoop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 0; i < 5; i++) //for dan pernyataan didalamnya
+    {
+        cout << i << endl; // code yang dieksekusi
+    }
+}
+
+/**
+ for (statement 1; statement 2; statement n...) {
+  // code block to be executed
+}
+ *
+ */
