5e89c611498947298db6af7de8dae56d47cc9719
@suriv27121994 <suriv2712194@gmail.com>
2021-12-28 08:44:20 +0800

    add math (matematika

---
+++ c-develop/26.Math/math.cpp
@@ -0,0 +1,16 @@
+// Matematika C++
+// C++ memiliki banyak fungsi yang memungkinkan Anda untuk melakukan tugas matematika pada angka.
+#include <iostream>
+#include <cmath> // Include the cmath library
+using namespace std;
+int main()
+{
+    cout << "Nilai Min: " << min(3, 10) << endl;
+    cout << "Nilai Max: " << max(10, 15) << endl;
+    cout << "Nilai Sqrt: " << sqrt(64) << endl;
+    cout << "Nilai Round: " << round(2.6) << endl;
+    cout << "Nilai Log: " << log(2) << endl;
+}
+
+// note
+// Dan fungsi min(x,y) dapat digunakan untuk mencari nilai terendah dari x dan y:
