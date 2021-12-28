a2ea5daf50545041ff5bc46556dae5ae1b763da4
@suriv27121994 <suriv2712194@gmail.com>
2021-12-28 08:08:37 +0800

    add file and image call function

---
+++ c-develop/24. While Loop/whileLoop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 0;     // initial variable dan value nya
+    while (i < 10) // condition
+    {
+        // blok kode yang dieksekusi
+        cout << i << endl;
+        i++;
+    }
+}
