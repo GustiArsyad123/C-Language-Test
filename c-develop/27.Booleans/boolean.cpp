0be80eaff467638777056cde75957d46a5fe1897
@suriv27121994 <suriv2712194@gmail.com>
2021-12-28 08:53:29 +0800

    add the booleans

---
+++ c-develop/27.Booleans/boolean.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    bool thisTrue = true;
+    bool thisFalse = false;
+    cout << "Valeu 1/True: " << thisTrue << "\n";  // Outputs 1 (true)
+    cout << "Valeu 0/False: " << thisFalse << "\n";    // Outputs 0 (false)
+}
